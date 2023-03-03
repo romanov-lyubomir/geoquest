3befffcd29591983c85e821b3155a0e709557d4d
romanov-lyubomir <romanov.lyubomir+github@protonmail.com>
2023-03-03 19:55:21 +0200

    Commit

--- WinFormsApp1/WinFormsApp1/Models/Users.cs
+++ WinFormsApp1/WinFormsApp1/Models/Users.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace GeoQuest.Models
 {
     internal class Users
     {
-        
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
     }
 }
